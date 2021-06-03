4aab05376c049b39d5d374eaf067eef10d43926a
CarolFLima <carolflima0@gmail.com>
2021-06-03 16:41:47 -0300

    add config env file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    secret_key = app.config.get("SECRET_KEY")
+    return f"The configured secret key is {secret_key}."
