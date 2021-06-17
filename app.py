c856e6fa86bd9fe531719a20b1da54b24d0bf675
CarolFLima <carolflima0@gmail.com>
2021-06-17 20:53:39 -0300

    setting up redis connection

--- app.py
+++ app.py
@@ -79,12 +79,20 @@ def index():
         job = q.enqueue_call(
             func=count_and_save_words, args=(url,), result_ttl=5000
         )
-        print(job.get_id())
+        print(f'ID: -- {job.get_id()}')
 
     return render_template('index.html', results=results)
 
 
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
 
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Nay!", 202
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'this-is-the-default-key')
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    
+   
 
 class ProductionConfig(Config):
     pass
